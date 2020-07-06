4940ddff60bc04b111d98cab846275b852d281a5
huynhthetong280619 <huynhthetong1999999@gmail.com>
2020-07-06 16:59:50 +0700

    Update handle events and UI

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { get } from 'lodash';
+import { get, reduce } from 'lodash';
 import {
   StyleSheet,
   View,
@@ -15,7 +15,7 @@ import {
 } from 'react-native';
 
 // Utils
-import { getCurrentAndNextTemperater, getPredictTemprature } from './utils/api';
+import { getCurrentAndNextTemperater, getPredictTemprature, getPredict,getUpdateModelPredict } from './utils/api';
 
 // Source icon
 import background from './assets/forest.jpg';
@@ -23,6 +23,8 @@ import tempbg from './assets/temp.png';
 import humbg from './assets/hum.png';
 import temp from './assets/temperature.png';
 import humidity from './assets/humidity.png';
+import update from './assets/update.png';
+import maths from './assets/maths.png';
 // Main component
 export default class App extends React.Component {
   constructor(props) {
@@ -32,9 +34,9 @@ export default class App extends React.Component {
       currentTemprature: null,
       currentHumid: null,
       predictTemprature: null,
-      predictHumid: null,
       predictTemperatureBaseInput: null,
-      inputValue: null,
+      inputValueTemperature: null,
+      inputValueHumid: null,
       isLoading: false
     }
 
@@ -43,7 +45,7 @@ export default class App extends React.Component {
   }
 
   handleLoopCycleUpdate() {
-    setInterval(() => this.serverRender(), 6000);
+    setInterval(() => this.serverRender(), 60000);
   }
 
   componentDidMount() {
@@ -54,35 +56,50 @@ export default class App extends React.Component {
   serverRender = async () => {
     this.setState({}, async () => {
       const resData = await getCurrentAndNextTemperater();
+      const resNData = await getPredict()
+      
+      console.log('Server', resNData);
       this.setState({
         currentTemprature: get(resData, 'currentTemprature'),
-        predictTemprature: get(resData, 'predictTemprature'),
-        currentHumid: get(resData, 'currentHumid'),
-        predictHumid: get(resData, 'predictHumid')
+        predictTemprature: get(resNData, 'next'),
+        currentHumid: get(resData, 'currentHumid')
       })
     })
   }
 
-  handleInput = (value) => {
+  handleInputTemperature = (value) => {
     let regrexCheck = value.match(/^\d+$/);
     console.log('regrex check', regrexCheck);
     regrexCheck ? this.setState({
-      inputValue: value
-    }) : this.setState({ inputValue: null });
+      inputValueTemperature: value
+    }) : this.setState({ inputValueTemperature: null });
   }
 
+  handleInputHumid = (value) => {
+    let regrexCheck = value.match(/^\d+$/);
+    console.log('regrex check', regrexCheck);
+    regrexCheck ? this.setState({
+      inputValueHumid: value
+    }) : this.setState({ inputValueHumid: null });
+  }
+
+
   getCalNextTemperature = () => {
-    if (!this.state.inputValue) {
+    if (!this.state.inputValueHumid || !this.state.inputValueTemperature) {
       return;
     }
     this.setState({ isLoading: true })
     this.setState({}, async () => {
-      const { next } = await getPredictTemprature(this.state.inputValue);
+      const { next } = await getPredictTemprature(this.state.inputValueTemperature, this.state.inputValueHumid);
       this.setState({
         predictTemperatureBaseInput: next,
         isLoading: false
       });
     })
+  }
+
+  getUpdateModel = async () =>{
+    await getUpdateModelPredict();
   }
 
   render() {
@@ -123,38 +140,42 @@ export default class App extends React.Component {
               <View style={{ flex: 1, flexDirection: 'column' }}>
                 <Text style={{ color: "#ffffff" }}>Current</Text>
                 <View style={{ width: 100, height: 50, borderColor: '#f1f1f1', borderWidth: 2, margin: 2 }}>
-                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33 }}>
-                    <Image source={temp} style={{ borderWidth: 2 }} />{this.state.currentTemprature} °C
+                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33, paddingLeft:10 }}>
+                    <Image source={temp} style={{ borderWidth: 2 }} />    {this.state.currentTemprature} °C
                   </Text>
                 </View>
                 <View style={{ width: 100, height: 50, borderColor: '#f1f1f1', borderWidth: 2, margin: 2 }}>
-                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33 }}>
-                    <Image source={humidity} style={{ borderWidth: 2 }} />{this.state.currentHumid} %
+                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33, paddingLeft:10 }}>
+                    <Image source={humidity} style={{ borderWidth: 2 }} />    {this.state.currentHumid} %
                   </Text>
                 </View>
               </View>
               <View style={{ flex: 1, flexDirection: 'column' }}>
                 <Text style={{ color: "#ffffff" }}>Predictional</Text>
                 <View style={{ width: 100, height: 50, borderColor: '#f1f1f1', borderWidth: 2, margin: 2 }}>
-                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33 }}>
-                    <Image source={temp} style={{ borderWidth: 2 }} />{this.state.predictTemprature} °C
+                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33, paddingLeft:10 }}>
+                    <Image source={temp} style={{ borderWidth: 2 }} />    {this.state.predictTemprature} °C
                   </Text>
                 </View>
                 <View style={{ width: 100, height: 50, borderColor: '#f1f1f1', borderWidth: 2, margin: 2 }}>
-                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33 }}>
-                    <Image source={humidity} style={{ borderWidth: 2 }} />{this.state.predictHumid} %
+                  <Text style={{ color: '#ffffff', height: '100%', lineHeight: 33, paddingLeft:10 }}>
+                    <Image source={maths} style={{ borderWidth: 2 }} />    {this.state.predictTemperatureBaseInput} °C
                   </Text>
                 </View>
               </View>
             </View>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',
-               position: 'absolute', bottom: '10%', left: '20%' }}>
-              <TextInput onChange={(e) => this.handleInput(e.nativeEvent.text)}
-                style={{ height: 40, borderWidth: 2, borderColor: '#ffffff', marginVertical: 10,
+               position: 'absolute', bottom: '5%', left: '20%' }}>
+              <TextInput onChange={(e) => this.handleInputTemperature(e.nativeEvent.text)}
+                style={{ height: 30, borderWidth: 2, borderColor: '#ffffff', marginVertical: 10,
                 paddingHorizontal: 10, color: '#ffffff' }}
-                placeholder="Type here any temperature you want..." />
+                placeholder="Type here any temperature you want ..." />
+              <TextInput onChange={(e) => this.handleInputHumid(e.nativeEvent.text)}
+                style={{ height: 30, borderWidth: 2, borderColor: '#ffffff', marginVertical: 10,
+                paddingHorizontal: 10, color: '#ffffff' }}
+                placeholder="Type here any humid you want..." />
               <TouchableOpacity onPress={() => this.getCalNextTemperature()}
-                style={{ height: 40, width: '50%', borderRadius: 40, backgroundColor: '#841584',
+                style={{ height: 40, width: '60%', borderRadius: 40, backgroundColor: '#841584',
                 justifyContent: 'center', alignItems: 'center' }}>
                 {this.state.isLoading ? <ActivityIndicator animating={this.state.isLoading} /> :
                   (<Text style={{ color: '#ffffff', fontWeight: 'bold' }}>GET FUTURE</Text>)}
@@ -162,9 +183,14 @@ export default class App extends React.Component {
             </View>
             {this.state.predictTemperatureBaseInput &&
               <Text style={{ fontSize: 15, color: '#ffffff', marginBottom: 1, position: 'absolute',
-                bottom: 20, left: "30%", fontWeight: 'bold' }}>
+                bottom: 10, left: "30%", fontWeight: 'bold' }}>
                 The temperature next is: {this.state.predictTemperatureBaseInput} °C
             </Text>}
+          </View>
+          <View style={{flex:1, position:'absolute', right:'5%', top:"5%", borderRadius:100, backgroundColor:'#ffffff'}}>
+            <TouchableOpacity onPress={()=>this.getUpdateModel()}>
+                <Image source={update} style={{width:24, height:24}}/>
+            </TouchableOpacity>
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
--- utils/api.js
+++ utils/api.js
@@ -1,21 +1,25 @@
-const axios = require('axios');
+   export const getCurrentAndNextTemperater = async () =>{
+      console.log('IN');
+      const response = await fetch(`http://flask-api-s-env.eba-q4x4enz2.us-east-2.elasticbeanstalk.com/iot`);
+      const r = await response.json();
+      console.log('TEXT', r);
+      return r;
+   }
 
-export const getCurrentAndNextTemperater = async () =>{
-   console.log('IN');
-   const response = await fetch(`http://flask-env.eba-t3v3cmsz.us-east-2.elasticbeanstalk.com/iot`);
-   const r = await response.json();
-   return r;
-}
+   export const getPredictTemprature = async (temp, humid) => {
+      const response = await fetch(`http://flask-api-s-env.eba-q4x4enz2.us-east-2.elasticbeanstalk.com/iot/${Number(temp).toFixed(1)}/${Number(humid).toFixed(1)}`);
+      const r = await response.json();
+      return r;
+   }
 
-export const getPredictTemprature = async (temp) => {
-   const response = await fetch(`http://flask-env.eba-t3v3cmsz.us-east-2.elasticbeanstalk.com/iot/${Number(temp).toFixed(1)}`);
-   const r = await response.json();
-   return r;
-}
 
-export const getTempratureBaseOther = async (temp) =>{
-   const response = await fetch(`https://cloudcomputing7.herokuapp.com/iot/${temp}`)
-   const r = await response.json();
-   return null;
-}
+   export const getPredict = async () => {
+      const response = await fetch(`http://flask-api-s-env.eba-q4x4enz2.us-east-2.elasticbeanstalk.com/iot/next`);
+      const r = await response.json();
+      console.log('Next', r);
+      return r;
+   }
 
+   export const getUpdateModelPredict = async () =>{
+      return null;
+   }
